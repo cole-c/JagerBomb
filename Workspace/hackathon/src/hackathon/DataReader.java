644d5585e3bb673e0336b61b2d5e8c6d82d4a5db
cole-c <cole@iastate.edu>
2015-07-18 17:34:38 -0500

    All the errors are gone, along with 8 hours of my fucking life

--- Workspace/hackathon/src/hackathon/DataReader.java
+++ Workspace/hackathon/src/hackathon/DataReader.java
@@ -5,6 +5,9 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DataReader {
 
 	static Integer count = 0;
@@ -22,6 +25,8 @@ public class DataReader {
 			s.nextLine();
 			count++;
 			s.useDelimiter(",");
+			if(!s.hasNextInt())
+				break;
 			Integer eventId = s.nextInt();
 			Integer eventTime = s.nextInt();
 			String producerType = s.next();
@@ -32,9 +37,37 @@ public class DataReader {
 			Integer outputVersion = s.nextInt();
 			s.useDelimiter("\n");
 			String outputData = s.next();
-			outputData = outputData.substring(1, outputData.lastIndexOf("}"));
-			System.out.println(outputData);
-			processJSON(outputData); //temporarily void until outputData is defined
+			outputData = outputData.substring(2, outputData.lastIndexOf("}"));
+			//System.out.println(outputData);
+			outputData = outputData.replace("\"\"\"", "\"");
+			outputData = outputData.replace("\"\"", "\"");
+			outputData = outputData.replace("\"bill", "bill");
+			outputData = outputData.replace("\"customer", "customer");
+			outputData = outputData.replace("\"route", "route");
+			outputData = outputData.replace("\"name", "name");
+			outputData = outputData.replace("\"city", "city");
+			outputData = outputData.replace("\"origin", "origin");
+			outputData = outputData.replace("\"state", "state");
+			outputData = outputData.replace("\"destination", "destination");
+			outputData = outputData.replace("\"location", "location");
+			outputData = outputData.replace("\"current", "current");
+			outputData = outputData.replace("}\"", "}");
+			outputData = outputData.replace(",\"", ",");
+			outputData = outputData + "}";
+			//System.out.println(outputData);
+			
+			try {
+				JSONObject json = new JSONObject(outputData);
+				//System.out.print(json.getString("type")+" ");
+				//System.out.print(json.getInt("id")+" ");
+				//System.out.println(json.getInt("version")+" ");
+				String type = json.getString("type");
+			} catch (JSONException e) {
+				//shouldn't ever happen with properly formatted data
+				e.printStackTrace();
+			}
+			
+			//processJSON(outputData); //temporarily void until outputData is defined
 			Event event = new Event(eventId, eventTime, producerType, producerId, producerVersion, outputType, outputId, outputVersion, outputData);
 			eventHash.put(eventId, event);
 		}
